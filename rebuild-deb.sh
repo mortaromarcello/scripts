2eb6340ccf40c09ad34ee092afe9eb76c23d368f
mortaromarcello <mortaromarcello@gmail.com>
2013-09-20 10:04:32 +0200

    Update rebuild-deb.sh

--- rebuild-deb.sh
+++ rebuild-deb.sh
@@ -4,5 +4,11 @@ if [ -z $1 ]; then
   echo "$0 <namefile.deb>"
   exit
 fi
-
-ar rcs $1 debian-binary data.tar.gz control.tar.gz
+if [ -e data.tar.gz ] && [ -e control.tar.gz ]; then
+  if [ ! -e debian-binary ]; then
+    echo 2.0 > debian-binary
+  fi
+  ar rcs $1 debian-binary data.tar.gz control.tar.gz
+elif
+  echo "data.tar.gz or control.tar.gz missing."
+fi
