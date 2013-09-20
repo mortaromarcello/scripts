c03d4ce581de9a3fa6167a462b66a59d7e4beb55
mortaromarcello <mortaromarcello@gmail.com>
2013-09-20 10:41:58 +0200

    modified:   extract-deb.sh 	modified:   rebuild-deb.sh

--- extract-deb.sh
+++ extract-deb.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 if [ -z $1 ]; then
-  echo "$0 <namefile.deb>"
+  echo "$0 <namefile.deb> [<data-dir>] [data-control>]"
   exit
 fi
 
+DATA="data"
+CONTROL="control"
+
+if [ ! -z $2 ]; then
+  DATA=$2
+fi
+if [ ! - $3 ]; then
+  CONTROL=$3
+fi
+
 ar vx $1
+mkdir -p $DATA
+tar -C $DATA -xzvf data.tar.gz
+mkdir -p $CONTROL
+tar -C $CONTROL -xzvf control.tar.gz
+
--- rebuild-deb.sh
+++ rebuild-deb.sh
@@ -1,14 +1,38 @@
-#!/usr/bash
+#!/bin/bash
 
 if [ -z $1 ]; then
-  echo "$0 <namefile.deb>"
+  echo "$0 <namefile.deb> [<data-dir>] [<control-dir>]"
   exit
 fi
-if [ -e data.tar.gz ] && [ -e control.tar.gz ]; then
-  if [ ! -e debian-binary ]; then
-    echo 2.0 > debian-binary
-  fi
-  ar rcs $1 debian-binary data.tar.gz control.tar.gz
-elif
-  echo "data.tar.gz or control.tar.gz missing."
+
+DATA="data"
+CONTROL="control"
+if [ ! -e $2 ]; then
+  DATA=$2
 fi
+if [ ! -e $3 ]; then
+  CONTROL=$3
+fi
+
+if [ -d $DATA ]; then
+  cd $DATA
+  tar -czvf ../data.tar.gz *
+  cd ../
+else
+  echo "$DATA not exist."
+  exit
+fi
+
+if [ -d $CONTROL ]; then
+  cd $CONTROL
+  tar -czvf ../control.tar.gz *
+  cd ../
+else
+  echo "$CONTROL not exist."
+  exit
+fi
+
+if [ ! -e debian-binary ]; then
+  echo 2.0 > debian-binary
+fi
+ar rcs $1 debian-binary data.tar.gz control.tar.gz
