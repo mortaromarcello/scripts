dce3f81cac25f54a08ad58dd5e8062237c964331
mortaromarcello <mortaromarcello@gmail.com>
2013-11-02 12:28:33 +0100

    modificato:   simple_install_distro.sh

--- simple_install_distro.sh
+++ simple_install_distro.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-# semplice script di installazione
 #
+# semplice script di installazione
 #
 #-----------------------------------------------------------------------
 #  inserire qui le label
+
 DISTRO="distro"
 INST_PARTITION="/dev/sda1"
-HOME_PARTITION="/dev/sda2"
+HOME_PARTITION=
 INST_ROOT_DIRECTORY="/mnt/${DISTRO}"
-LIST_CP_DIRECTORY=(bin boot etc lib opt sbin srv usr var)
 TYPE_FS="ext4"
+USER=
+CRYPT_PASSWORD=
 
 #-----------------------------------------------------------------------
+
+function check_root() {
+  if [ $UID != 0 ]; then
+    echo "Devi essere root per eseguire questo script."
+    exit
+  fi
+}
 
 function check_hd() {
 	echo "work"
@@ -19,57 +28,43 @@ function check_hd() {
 
 get_info() {
   echo "Partizione di installazione (root):" $INST_PARTITION
-  echo "Partizione di home                :" $HOME_PARTITION
+  if [ ! -z $HOME_PARTITION ]; then
+    echo "Partizione di home                :" $HOME_PARTITION
+  fi
   echo "Tipo di file system               :" $TYPE_FS
-  echo "Username                          :" $USER
-  echo "Password criptata                 :" $CRYPT_PASSWORD
+  if [ ! -z $USER ]; then
+    echo "Username                          :" $USER
+  fi
+  if [ ! -z $CRYPT_PASSWORD ]; then
+    echo "Password criptata                 :" $CRYPT_PASSWORD
+  fi
 }
 
-function cp_distro() {
-	echo "work"
-    for dir in $LIST_CP_DIRECTORY; do
-        cp -a "/$dir" "$INST_ROOT_DIRECTORY"
-    done
-}
-
-function setup_distro {
-    echo "work"
-    read -p "Digita la partizione d'installazione(/dev/sda1):" INST_PARTITION
-    read -p "Digita il tipo di file system (ext2/3/4):" TYPE_FS
+function run_inst {
     read -p "Attenzione! la partizione sarà formattata! (Ctrl-c per abortire)"
     mkfs -t $TYPE_FS $INST_PARTITION
-    mount $INST_PARTITION $INST_ROOT_DIRECTORY
-    if [ $? -ne 0 ]; then
-      echo "Problemi!!!"
-      exit
-    fi
     mkdir -p $INST_ROOT_DIRECTORY
-    cd $INST_ROOT_DIRECTORY
-    mount -t proc proc proc/
-    mount -t sysfs sys sys/
-    mount -o bind /dev dev/
-    mount -t devpts pts dev/pts/
-    chroot . /bin/bash
-}
-
-function add_user() {
-  if [ -z $USER ]; then
-    read -p "Digita la username: " USER
-  fi
-  if [ -z $CRYPT_PASSWORD ]; then
-    read -s -p "Digita la password: " USER_PASSWORD
-    CRYPT_PASSWORD=$(perl -e 'print crypt($ARGV[0], "password")' $USER_PASSWORD)
-  fi
-  useradd -m -p $CRYPT_PASSWORD $USER
-  [ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!";exit
+    mount $INST_PARTITION $INST_ROOT_DIRECTORY
+    if [ -z $HOME_PARTITION ]; then
+      mkfs -t $TYPE_FS $HOME_PARTITION
+      mkdir -p "${INST_ROOT_DIRECTORY}/home"
+      mount $HOME_PARTITION "$INST_ROOT_DIRECTORY/home"
+    fi
+    for dir in bin boot etc lib opt sbin srv usr var; do
+      cp -av "/$dir" "$INST_ROOT_DIRECTORY"
+    done
+    if [ -z $USER ]; then
+      read -p "Digita la username: " USER
+    fi
+    if [ -z $CRYPT_PASSWORD ]; then
+      read -s -p "Digita la password: " USER_PASSWORD
+      CRYPT_PASSWORD=$(perl -e 'print crypt($ARGV[0], "password")' $USER_PASSWORD)
+    fi
+    #chroot $INST_ROOT_DIRECTORY useradd -m -p $CRYPT_PASSWORD $USER
+    #[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!";exit
 }
 
 #------------------------------------------------------------------------
-#check_args
-#check_hd
-#setup_distro
-#cp_distro
-#echo ${#}
 
 until [ -z "$1" ]
 do
@@ -101,5 +96,6 @@ do
       ;;
   esac
 done
-get_info
 
+check_root
+get_info
