9d6a552dad99b44ff8c7920a9c5860f7be1c5d6f
mortaromarcello <mortaromarcello@gmail.com>
2013-12-18 18:44:10 +0100

    modified:   yad_install_distro.sh

--- yad_install_distro.sh
+++ yad_install_distro.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-PARTITIONS=()
 PARTITIONS="$(awk '{if ($4 ~ /[hs]d[a-z][1-9]/) print $4}' /proc/partitions)"
-
 for item in $PARTITIONS; do 
   PART_ARRAY+=($item)
 done
-#partitionslist="Menu"!
 length=$((${#PART_ARRAY[@]} - 1))
-echo $length
-last_par=${PART_ARRAY[${length}]}
-echo $last_par
-for item in $PARTITIONS; do
-  #partitionslist+="^"
-  [ ! $item=$last_par ] && partitionslist+="${item}"! || partitionslist+="${item}"
+for (( i=0; i <= length; i++ )); do
+  [[ i -lt length ]] && partitionslist+="${PART_ARRAY[$i]}"! || partitionslist+="${PART_ARRAY[$i]}";
 done
 DISKS="$(awk '{if (length($4)==3 && $4 ~ /[hs]d/) print $4}' /proc/partitions)"
-for item in $DISKS; do
-  #diskslist+="^"
-  diskslist+="${item}"!
+for item in $DISKS; do DISKS_ARRAY+=($item); done
+length=$((${#DISKS_ARRAY[@]} - 1))
+for (( i=0; i <= length; i++ )); do
+  [[ i -lt length ]] && diskslist+="${DISKS_ARRAY[$i]}"! || 
+  diskslist+="${DISKS_ARRAY[$i]}"
 done
 MOUNTS_SYSTEM=("/" "home" "var" "boot")
 #echo $PARTITIONS $DISKS ${MOUNTS_SYSTEM[3]}
