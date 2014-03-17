7434cc386faef2edfa4c149d1f81b0cc1211f067
mortaromarcello <mortaromarcello@gmail.com>
2014-03-17 19:01:41 +0100

    modified:   install_distro.py

--- install_distro.py
+++ install_distro.py
@@ -382,17 +382,23 @@ class MyFrame(wx.Frame):
     self.SetStatusText('Inizio installazione...')
     if not self.checkRequisites():
       #---- codice controllo
-      pass
-      #self.installa.Enable()
-      #return
+      self.installa.Enable()
+      return
       #----
     self.panel.Hide()
     self.panel_info.Show()
     self.sizer.Fit(self)
-    self.panel_info.updateInfo(" Nome Distro:\t\t\t\t%s\n Root partition:\t\t\t%s\n UUID root partition:\t\t%s\n Home partition:\t\t\t%s\n Format home:\t\t\t%s\n Autologin:\t\t\t\t%s\n UUID home partition:\t\t%s\n Swap partition:\t\t\t%s\n UUID swap partition:\t\t%s\n Drive installation:\t\t\t%s\n Directory root installation:\t%s\n Name User:\t\t\t\t%s\n Crypt user password:\t\t%s\n Crypt root password:\t\t%s\n Locale:\t\t\t\t\t%s\n Lang:\t\t\t\t\t%s\n Keyboard:\t\t\t\t%s\n Hostname:\t\t\t\t%s\n Groups:\t\t\t\t\t%s\n Timezone:\t\t\t\t%s\n Shell user:\t\t\t\t%s\n" % (Glob.DISTRO, Glob.ROOT_PARTITION, Glob.UUID_ROOT_PARTITION, Glob.HOME_PARTITION, Glob.FORMAT_HOME, Glob.AUTOLOGIN, Glob.UUID_HOME_PARTITION, Glob.SWAP_PARTITION, Glob.UUID_SWAP_PARTITION, Glob.INST_DRIVE, Glob.INST_ROOT_DIRECTORY, Glob.USER, Glob.CRYPT_USER_PASSWORD, Glob.CRYPT_ROOT_PASSWORD, Glob.LOCALE, Glob.LANG, Glob.KEYBOARD, Glob.HOSTNAME, Glob.GROUPS, Glob.TIMEZONE, Glob.SHELL_USER))
+    info = " Nome Distro:\t\t\t\t%s\n Root partition:\t\t\t%s\n UUID root partition:\t\t%s\n Home partition:\t\t\t%s\n Format home:\t\t\t%s\n Autologin:\t\t\t\t%s\n UUID home partition:\t\t%s\n Swap partition:\t\t\t%s\n UUID swap partition:\t\t%s\n Drive installation:\t\t\t%s\n Directory root installation:\t%s\n Name User:\t\t\t\t%s\n Crypt user password:\t\t%s\n Crypt root password:\t\t%s\n Locale:\t\t\t\t\t%s\n Lang:\t\t\t\t\t%s\n Keyboard:\t\t\t\t%s\n Hostname:\t\t\t\t%s\n Groups:\t\t\t\t\t%s\n Timezone:\t\t\t\t%s\n Shell user:\t\t\t\t%s\n" % (Glob.DISTRO, Glob.ROOT_PARTITION, Glob.UUID_ROOT_PARTITION, Glob.HOME_PARTITION, Glob.FORMAT_HOME, Glob.AUTOLOGIN, Glob.UUID_HOME_PARTITION, Glob.SWAP_PARTITION, Glob.UUID_SWAP_PARTITION, Glob.INST_DRIVE, Glob.INST_ROOT_DIRECTORY, Glob.USER, Glob.CRYPT_USER_PASSWORD, Glob.CRYPT_ROOT_PASSWORD, Glob.LOCALE, Glob.LANG, Glob.KEYBOARD, Glob.HOSTNAME, Glob.GROUPS, Glob.TIMEZONE, Glob.SHELL_USER)
+    self.panel_info.updateInfo(info)
+    result = wx.MessageDialog(None, _("Attenzione! L'installazione formatterà la/le partizione/i del/dei disco/i! Sei Sicuro?"), _("Attenzione"), wx.YES_NO|wx.ICON_QUESTION).ShowModal()
+    if result == wx.ID_NO: return
     self.runInst = True
     self.createRootAndMountPartition()
+    self.panel_info.updateInfo(info)
     self.createHomeAndMountPartition()
+    self.panel_info.updateInfo(info)
+    self.createSwapPartition()
+    self.panel_info.updateInfo(info)
     self.copyRoot()
     self.addUser()
     self.changeRootPassword()
@@ -462,6 +468,16 @@ class MyFrame(wx.Frame):
       Glob.PROC_ERROR = proc.returncode
       if Glob.PROC_ERROR: self.checkError()
     else: self.checkError()
+  
+  def createSwapPartition(self):
+    """ """
+    if not Glob.SWAP_PARTITION: return
+    print 'createSwapPartition'
+    proc = runProcess("mkswap -c %s" % Glob.SWAP_PARTITION)
+    Glob.PROC_ERROR = proc.returncode
+    if not Glob.PROC_ERROR:
+      Glob.UUID_SWAP_PARTITION = blkid(Glob.SWAP_PARTITION)
+    if Glob.PROC_ERROR: self.checkError()
   
   def copyRoot(self):
     """ """
