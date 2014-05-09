07f824eec91154e529d558b4d727ad76406ef271
mortaromarcello <mortaromarcello@gmail.com>
2014-05-09 12:38:56 +0200

    modified:   extract-po.sh

--- extract-po.sh
+++ extract-po.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-[ ! ${1} ] && exit
-LANGUAGE="it"
+if [ ! ${2} ]; then
+	echo "${0} <file(without extension)> <language>"
+	exit
+fi
+LANGUAGE=${2}
 OUTDIR="./locale/${LANGUAGE}/LC_MESSAGES"
 [ ! -d ${OUTDIR} ] && mkdir -p -v ${OUTDIR}
 xgettext -d ${1} -p ${OUTDIR} ${1}.py
