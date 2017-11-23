b171060f37340453362f42e527dc72dd7e0f7b0f
mortaromarcello <mortaromarcello@gmail.com>
2017-11-23 14:15:52 +0000

    modified:   simple_install_distro.sh

--- simple_install_distro.sh
+++ simple_install_distro.sh
@@ -49,9 +49,10 @@ Installa la Live su un disco.
 	-a | --autologin <true/false>          :Autologin utente. (default 'true').
 	-c | --crypt-password <password>       :Password cifrata utente.
 	-C | --crypt-root-password <password>  :Password cifrata root.
-	-d | --inst-drive <drive>              :Drive di installazione per grub.
+	-d | --inst-drive <drive>              :Drive di installazione.
 	-f | --format-home                     :Formatta la partizione home.
 	-g | --groups <group1,...,groupn>      :Gruppi addizionali a cui l'utente appartiene.
+	-G | --grub-drive                      :Drive di installazione per grub.
 	-h | --help                            :Stampa questa messaggio.
 	-H | --home-partition <partition>      :Partizione di home.
 	-i | --inst-root-directory <directory> :Directory di installazione (default '/mnt/distro').
@@ -119,6 +120,7 @@ put_info() {
 	echo "Lingua                            :" "${LANG}"
 	echo "Timezone                          :" "${TIMEZONE}"
 	echo "Shell user                        :" "${SHELL_USER}"
+	read -r
 }
 
 function create_root_and_mount_partition() {
